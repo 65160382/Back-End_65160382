29761449d18fdda5861b3dac71617eb6ddc927e7
Piyawat Seepattha <65160382@go.buu.ac.th>
2024-08-05 13:41:22 +0700

    3.1

---
+++ 3.1 Express Server/index.js
@@ -0,0 +1,10 @@
+import express from "express"
+const server = express();
+// port
+const port=3000;
+
+
+//server run on port 3000 and callback
+server.listen(port,() =>{
+    console.log(`Server running on port ${port}.`); // callback
+})
