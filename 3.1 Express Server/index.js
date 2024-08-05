286bb54a6635843a6024a31253fc18c8b68b354c
Piyawat Seepattha <65160382@go.buu.ac.th>
2024-08-05 14:47:58 +0700

    finish
    5 /8/2567

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -3,8 +3,11 @@ const server = express();
 // port
 const port=3000;
 
-
 //server run on port 3000 and callback
 server.listen(port,() =>{
     console.log(`Server running on port ${port}.`); // callback
 })
+
+
+
+
